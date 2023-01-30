bd20d8e69e0d4492796b93bd5d21745de3342a29
Hafidev <hafidev@hafidevs-iMac.lan>
2023-01-30 23:37:48 +0000

    fmt

--- app/app/settings.py
+++ app/app/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-s$&-1)y3z1%7_j+bugp6hc!7dzk4otr==b_*eoz4t1&+h$@xlc'
